3cd5f21865385be2ec2394a22f4a5a7c2aeacdd5
Matthieu Vachon <matthieu.o.vachon@gmail.com>
2012-10-17 21:18:31 -0400

    Parameters is now wrapped in a module

--- lib/nugrant/parameters.rb
+++ lib/nugrant/parameters.rb
@@ -1,58 +1,63 @@
 require 'deep_merge'
-require 'nugrant/parameter_bag'
 require 'ostruct'
 
-class Nugrant::Parameters < Nugrant::ParameterBag
-  @config = nil
-  @parameters = nil
-  @local_parameters = nil
-  @global_parameters = nil
+module Nugrant
+  class Parameters < Nugrant::ParameterBag
+    @config = nil
+    @parameters = nil
+    @local_parameters = nil
+    @global_parameters = nil
 
-  def initialize(config)
-    @config = config
-    @parameters = load_parameters()
+    def initialize(config = nil)
+      if config == nil
+        config = Nugrant::Config.new()
+      end
 
-    super(@parameters)
-  end
+      @config = config
+      @parameters = load_parameters()
 
-  def get_bag()
-    @bag
-  end
-
-  def get_params()
-    return @parameters
-  end
-
-  def get_local_params()
-    return @local_parameters
-  end
-
-  def get_global_params()
-    return @global_parameters
-  end
-
-  def load_parameters()
-    @local_parameters = load_parameters_file(@config.local_params_path)
-    @global_parameters = load_parameters_file(@config.global_params_path)
-
-    if @local_parameters == nil and @global_parameters == nil
-      return nil
+      super(@parameters)
     end
 
-    parameters = Hash.new()
-    parameters.deep_merge!(@global_parameters) if @global_parameters != nil
-    parameters.deep_merge!(@local_parameters) if @local_parameters != nil
+    def get_bag()
+      @bag
+    end
 
-    return parameters
-  end
+    def get_params()
+      return @parameters
+    end
 
-  def load_parameters_file(file_path)
-    begin
-      File.open(file_path) do |file|
-        return YAML::load(file)
+    def get_local_params()
+      return @local_parameters
+    end
+
+    def get_global_params()
+      return @global_parameters
+    end
+
+    def load_parameters()
+      @local_parameters = load_parameters_file(@config.local_params_path)
+      @global_parameters = load_parameters_file(@config.global_params_path)
+
+      if @local_parameters == nil and @global_parameters == nil
+        return nil
       end
-    rescue
-      return nil
+
+      parameters = Hash.new()
+      parameters.deep_merge!(@global_parameters) if @global_parameters != nil
+      parameters.deep_merge!(@local_parameters) if @local_parameters != nil
+
+      return parameters
+    end
+
+    def load_parameters_file(file_path)
+      begin
+        File.open(file_path) do |file|
+          return YAML::load(file)
+        end
+      rescue
+        return nil
+      end
     end
   end
 end
