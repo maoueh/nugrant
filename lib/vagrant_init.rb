c40b74e430d07c2f7745881fbb91b08eeaf5aadb
Matthieu Vachon <matthieu.o.vachon@gmail.com>
2012-12-11 22:24:39 -0500

    Updated vagrant_init module  * Vagrant specific classes are now in a dedicated module.  * Add a user command that can be used to show the currently known parameters.

--- lib/vagrant_init.rb
+++ lib/vagrant_init.rb
@@ -1,33 +1,7 @@
 # This module will initialize the vagrant plugin
-require 'nugrant'
-
-module Nugrant
-  module Vagrant
-    class UserParameters < ::Vagrant::Config::Base
-      def initialize()
-        @parameters = Nugrant::Parameters.new()
-      end
-
-      def [](param_name)
-        return @parameters[param_name]
-      end
-
-      def method_missing(method, *args, &block)
-        return @parameters.method_missing(method, *args, &block)
-      end
-
-      def defaults(parameters)
-        @parameters.defaults(parameters)
-      end
-
-      def defaults=(parameters)
-        @parameters.defaults=(parameters)
-      end
-    end
-  end
-end
+require 'nugrant/vagrant/user_command'
+require 'nugrant/vagrant/user_parameters'
 
 # Plugin bootstrap
+Vagrant.commands.register(:user) { Nugrant::Vagrant::UserCommand }
 Vagrant.config_keys.register(:user) { Nugrant::Vagrant::UserParameters }
-
-
