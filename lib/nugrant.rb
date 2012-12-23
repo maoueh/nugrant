e68e31c7f2e91b73f06a628974e95bbb61f8ac8c
Matthieu Vachon <matthieu.o.vachon@gmail.com>
2012-12-23 12:11:46 -0500

    Fixed problems in ruby 1.8.7

--- lib/nugrant.rb
+++ lib/nugrant.rb
@@ -3,6 +3,11 @@ require 'nugrant/config'
 require 'nugrant/parameter_bag'
 require 'nugrant/parameters'
 
+unless defined?(KeyError)
+  class KeyError < IndexError
+  end
+end
+
 module Nugrant
   def self.create_parameters(options)
     config = Nugrant::Config.new(options)
