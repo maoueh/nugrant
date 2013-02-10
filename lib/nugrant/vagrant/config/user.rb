9c65797d13a01ae79344d604a7c6adfc944bd9f4
Matthieu Vachon <matthieu.o.vachon@gmail.com>
2013-02-09 23:37:24 -0500

    Moved vagrant user config to a dedicated namespace

---
+++ lib/nugrant/vagrant/config/user.rb
@@ -0,0 +1,29 @@
+require 'nugrant'
+
+module Nugrant
+  module Vagrant
+    module Config
+      class User < ::Vagrant::Config::Base
+        def initialize()
+          @parameters = Nugrant::Parameters.new()
+        end
+
+        def [](param_name)
+          return @parameters[param_name]
+        end
+
+        def method_missing(method, *args, &block)
+          return @parameters.method_missing(method, *args, &block)
+        end
+
+        def defaults(parameters)
+          @parameters.defaults(parameters)
+        end
+
+        def defaults=(parameters)
+          @parameters.defaults=(parameters)
+        end
+      end
+    end
+  end
+end
--- lib/vagrant_init.rb
+++ lib/vagrant_init.rb
@@ -1,7 +1,7 @@
 # This module will initialize the vagrant plugin
 require 'nugrant/vagrant/user_command'
-require 'nugrant/vagrant/user_parameters'
+require 'nugrant/vagrant/config/user'
 
 # Plugin bootstrap
 Vagrant.commands.register(:user) { Nugrant::Vagrant::UserCommand }
-Vagrant.config_keys.register(:user) { Nugrant::Vagrant::UserParameters }
+Vagrant.config_keys.register(:user) { Nugrant::Vagrant::Config::User }
