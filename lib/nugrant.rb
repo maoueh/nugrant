f54bac0faaac1d98f34588d57e28662a588fc372
Matthieu Vachon <matthieu.o.vachon@gmail.com>
2013-10-17 23:15:24 -0400

    Improved error reporting when a parameter cannot be found  * Fixes #8  * A error inheriting from VagrantError is raised with a nice    (an translatable) error message.

--- lib/nugrant.rb
+++ lib/nugrant.rb
@@ -7,7 +7,26 @@ unless defined?(KeyError)
   end
 end
 
+module Nugrant
+  def self.create_parameters(options)
+    config = Nugrant::Config.new(options)
+
+    return Nugrant::Parameters.new(config)
+  end
+
+  def self.setup_i18n()
+    I18n.load_path << File.expand_path("locales/en.yml", Nugrant.source_root)
+    I18n.reload!
+  end
+
+  def self.source_root
+    @source_root ||= Pathname.new(File.expand_path("../../", __FILE__))
+  end
+end
+
 if defined?(Vagrant)
+  Nugrant.setup_i18n()
+
   case
   when defined?(Vagrant::Plugin::V2)
     require 'nugrant/vagrant/v2/plugin'
@@ -15,13 +34,5 @@ if defined?(Vagrant)
     # Nothing to do, v1 plugins are picked by the vagrant_init.rb file
   else
     abort("You are trying to use Nugrant with an unsupported Vagrant version [#{Vagrant::VERSION}]")
-  end
-end
-
-module Nugrant
-  def self.create_parameters(options)
-    config = Nugrant::Config.new(options)
-
-    return Nugrant::Parameters.new(config)
   end
 end
