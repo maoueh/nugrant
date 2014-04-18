28479844b47d4935cacf24019c951a1eebf0ed0d
Matthieu Vachon <matthieu.o.vachon@gmail.com>
2014-04-17 21:53:16 -0400

    Fixed Vagrant User implementation  * We must warp mixin method_missing implementation within a    rescue block so KeyError are properly converted to Vagrant    exceptions.

--- lib/nugrant/vagrant/v2/config/user.rb
+++ lib/nugrant/vagrant/v2/config/user.rb
@@ -14,6 +14,15 @@ module Nugrant
           end
 
           include Mixin::Parameters
+
+          def method_missing(method, *args, &block)
+            module_method = Mixin::Parameters.instance_method(:method_missing)
+
+            # This calls method `method_missing` defined in Mixin::Parameters with self as the "called" instance
+            module_method.bind(self).call(method, *args, &block)
+          rescue KeyError
+            raise Errors::ParameterNotFoundError, :key => method.to_s
+          end
         end
       end
     end
